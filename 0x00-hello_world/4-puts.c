7114f8f4482208d5a82ff041ea711b8e4badaea3
mojtaba <mojtaba.mohamed187@gmail.com>
2023-08-10 08:46:28 -0700

    Add '4. Hello, puts' task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: A C program prints a sentence using the puts() function.
+ *
+ * Return: 0 if the program successed.
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
