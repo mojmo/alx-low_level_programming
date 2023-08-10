b2be3122df00a8235a64ac999b1670893175ca34
mojtaba <mojtaba.mohamed187@gmail.com>
2023-08-10 08:57:06 -0700

    Add '5. Hello, printf' task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include (stdio.h)
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: A C program prints a sentence using the printf() function.
+ *
+ * Return: 0 if the program successed.
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
