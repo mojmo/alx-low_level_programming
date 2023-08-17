357db89a389bb2f8c65e2dbb8309f44191ec3e5e
mojtaba <mojtaba.mohamed187@gmail.com>
2023-08-17 08:13:27 -0700

    Fix '9. Fizz-Buzz' task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,9 +17,9 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && !(i % 5 == 0))
 			printf("%s", "Fizz");
-		else if (i % 5 == 0)
+		else if ((i % 5 == 0) && !(i % 3 == 0))
 			printf("%s", "Buzz");
 		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("%s", "FizzBuzz");
