d874d176a554aa409fcb5ee9fa1590e24fff0391
mojtaba <mojtaba.mohamed187@gmail.com>
2023-08-23 10:07:27 -0700

    Fix '3. strcmp' task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,29 +8,21 @@
  * @s2: second string.
  *
  * Return: 0 if s1 equals s2.
- *         15 if s1 greater than s2.
- *         -15 if s1 less than s2.
+ *         positive number if s1 greater than s2.
+ *         negative number if s1 less than s2.
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int sum1 = 0;
-	int sum2 = 0;
 
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		if (s1[i] != '\0')
-			sum1 += s1[i];
-		if (s2[i] != '\0')
-			sum2 += s2[i];
+		if (s1[i] != s2[i])
+			break;
+
 		i++;
 	}
 
-	if (sum1 == sum2)
-		return (0);
-	if (sum1 > sum2)
-		return (15);
-	else
-		return (-15);
+	return (s1[i] - s2[i]);
 }
