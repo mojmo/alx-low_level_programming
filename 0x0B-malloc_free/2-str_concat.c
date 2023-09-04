cb3ba3f0f7298245af0ab362ce1abb4ee5b97a56
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-04 11:42:02 -0700

    fix returned value

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,6 +17,12 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	unsigned int len = 0, len1 = 0, len2 = 0, i = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[len1] != '\0')
 		len1++;
 
