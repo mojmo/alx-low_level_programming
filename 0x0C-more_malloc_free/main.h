8606a9156633b1269f76e71b3e030337ee1de1bb
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-11 07:58:18 -0700

    Add '0. Object-like Macro' task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _strlen(char *str);
+int _isdigit(int c);
+char *mult(char *s1, char *s2);
+int _putchar(char c);
 
 #endif
