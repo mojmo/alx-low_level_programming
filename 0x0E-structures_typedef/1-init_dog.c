c733b45690d41a6ea634b8ccee472576a93a97e4
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-11 12:15:01 -0700

    Add '1. A dog is the only thing on earth that loves you more than you love yourself' task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ *
+ * @d: pointer to the struct
+ * @name: a value to be assigned to a struct member
+ * @age: a value to be assigned to a struct member
+ * @owner: a value to be assigned to a struct member
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
