3983afc025b2b436165dcb08a33701662041ed81
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-13 09:54:12 -0700

    handle null pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,13 +2,13 @@
 
 /**
  * int_index - a function that searches for an integer.
- * 
+ *
  * @array: the array in which to search.
  * @size: the size of the array.
  * @cmp: a pointer to the function to be used to compare values.
  *
- * Return: the index of the first element for which the cmp function does not return 0.
- * If no element matches or size <= 0 return -1
+ * Return: the index of the first element for which the cmp function does
+ * not return 0. If no element matches or size <= 0 return -1
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -18,10 +18,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if(cmp(array[i]))
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
 
 	return (-1);
