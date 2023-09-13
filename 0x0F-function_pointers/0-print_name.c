5afeeb14f89899c1456b1c789859b8e2f177afe4
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-13 07:56:02 -0700

    Add '0. What's my name' task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name
+ * 
+ * @name: the name to be printed.
+ * @f(): a callback function get the name and print it.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
