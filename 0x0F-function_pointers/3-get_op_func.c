79dba765987b30cb52639985e96f0c270ab252f6
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-13 14:36:49 -0700

    change variable

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,7 +13,6 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	int i = 0;
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -22,6 +21,7 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	int i = 0;
 
 	while (i < 5)
 	{
