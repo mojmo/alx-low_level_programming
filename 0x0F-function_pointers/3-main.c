78284aa3cde6a91a4f2ab131be4e5bb40d09febd
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-13 14:28:12 -0700

    change a condition

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,9 +13,8 @@
  * Return: 0 if successed.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	char *op = argv[2];
 	int a = atoi(argv[1]), b = atoi(argv[3]);
 	int (*op_func)(int, int);
 
@@ -25,7 +24,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	op_func = get_op_func(op);
+	op_func = get_op_func(argv[2]);
 
 	if (!op_func)
 	{
@@ -33,7 +32,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*op == '/' || *op == '%') && !b)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && !b)
 	{
 		printf("Error\n");
 		exit(100);
