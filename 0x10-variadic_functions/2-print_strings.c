bfaecec18caf41e2508d437f9d2cbb313f110abe
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-14 09:09:29 -0700

    fix a condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (word)
 			printf("%s", word);
 		else
-			printf("nil");
+			printf("(nil)");
 
 		if (i != n - 1 && separator)
 			printf("%s", separator);
