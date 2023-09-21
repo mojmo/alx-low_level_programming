3179a7df5363de4a176101e6fd7027ee78bf1ea6
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-21 11:37:58 -0700

    Add '5. The Hare and the Tortoise' task

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,5 +29,6 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 unsigned int _strlen(char *s);
 void free_list(list_t *head);
+void first(void) __attribute__((constructor));
 
 #endif
