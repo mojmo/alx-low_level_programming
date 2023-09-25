14729fda63f26f2f097c2dd9f903945edbc9c959
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-25 10:55:00 -0700

    fix betty style

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - a function that adds a new node at the end of a listint_t list.
+ * add_nodeint_end - a function that adds a new node at the end of
+ * a listint_t list.
  *
  * @head: pointer to the head of the list.
  * @n: a value to be added in the the new node.
