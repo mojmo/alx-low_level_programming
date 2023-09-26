fa8ace137f865fd58e16b3cdbe8b74ca1ae4f648
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-26 14:06:02 -0700

    Add '12. Print (safe version)' task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ *
+ * @head: pointer to the head of the list.
+ *
+ * Return: the number of nodes in the list.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *currNode = (listint_t *)head;
+	unsigned int  count = 0;
+	long int diff;
+
+	while (currNode != NULL)
+	{
+		printf("[%p] %d\n", (void *)currNode, currNode->n);
+		count++;
+
+		/**
+		 * If the difference is negative or equal to zero, we are attempting to
+		 * access an address that we have previously visited.
+		 */
+		diff = currNode - currNode->next;
+
+		if (diff > 0)
+			currNode = currNode->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)currNode->next, currNode->next->n);
+			break;
+		}
+	}
+
+	return (count);
+}
