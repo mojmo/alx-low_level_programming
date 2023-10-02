a4bbf6ebcc77281d78794bce41d963604c468251
mojtaba <mojtaba.mohamed187@gmail.com>
2023-10-02 11:51:00 -0700

    change a condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,14 +36,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		wr = write(fd, "", 0);
-	else
+	if (text_content = NULL)
 		wr = write(fd, text_content, _strlen(text_content));
 
-	if (wr > 0)
-		return (1);
-
 	close(fd);
-	return (-1);
+	return (rw == -1 ? -1 : 1);
 }
