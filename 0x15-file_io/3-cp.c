613df5bb2f0928ce0da0921840a8b22f50f31d82
mojtaba <mojtaba.mohamed187@gmail.com>
2023-10-02 16:23:19 -0700

    Add '3. cp' task

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * cp - a function that copies the contents of one file to another file.
+ *
+ * @file_from: name of the file to copy from.
+ * @file_to: name of the file to copy to.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int cp(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to, rd, wr;
+	char buffer[BUFFER_SIZE];
+
+	if (file_from == NULL || file_to == NULL)
+		return (-1);
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, PERMS);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), close(fd_from), exit(99);
+	}
+	while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		wr = write(fd_to, buffer, rd);
+		if (wr != rd || wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
+	if (rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", errno), exit(100);
+	return (1);
+}
+
+/**
+ * main - Entry point of program.
+ *
+ * @argc: the number of command line arguments.
+ * @argv: the command line arguments.
+ *
+ * Return: 0 on success, -1 on failure.
+ */
+
+int main(int argc, char **argv)
+{
+	int res;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	res = cp(argv[1], argv[2]);
+
+	if (res != 1)
+		return (res);
+
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
+#define PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
