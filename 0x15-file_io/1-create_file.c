42dde5a6bf5a0c25bfe323dc21b514dc3f3ae22f
mojtaba <mojtaba.mohamed187@gmail.com>
2023-10-02 11:56:34 -0700

    fix an error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,9 +36,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content = NULL)
+	if (text_content != NULL)
 		wr = write(fd, text_content, _strlen(text_content));
 
 	close(fd);
-	return (rw == -1 ? -1 : 1);
+	return (wr == -1 ? -1 : 1);
 }
