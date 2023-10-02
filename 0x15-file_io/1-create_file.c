45f6ccd744c7e28794ec995ca8897d30a8ec2155
mojtaba <mojtaba.mohamed187@gmail.com>
2023-10-02 11:36:04 -0700

    Add '1. Under the snow' task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that prints a string, in reverse.
+ *
+ * @s: a pointer to the string.
+ *
+ * Return: the length of the string.
+ */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen(s + 1));
+}
+
+/**
+ * create_file - a function that creates a file.
+ *
+ * @filename: the name of the file to create.
+ * @text_content: a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	wr = write(fd, text_content, _strlen(text_content));
+	if (wr > 0)
+		return (1);
+
+	close(fd);
+	return (-1);
+}
